4557791687e649685a9818f813b499c334f1d146
robertIsaac <dev.robert.isaac@gmail.com>
2026-01-22 23:16:04 +0200

    chore: add proxy for the API to avoid CORS error

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  // TODO to be removed, this is just an example
+  verifyUser = httpResource(() => '/api/accounts/verify-username?userName=test');
+}
